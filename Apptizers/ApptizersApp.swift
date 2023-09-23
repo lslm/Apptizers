a055da446f425361548512e8219b549b5a647adb
Lucas S <lucas.santos.silva2011@gmail.com>
2023-09-23 15:32:04 -0300

    Project bootstrap

---
+++ Apptizers/ApptizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApptizersApp.swift
+//  Apptizers
+//
+//  Created by Lucas Santos on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ApptizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apptizers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apptizers
+//
+//  Created by Lucas Santos on 23/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
