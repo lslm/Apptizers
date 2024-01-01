73c078b249d7eff93d20ebc5c76f1f205ad7ec17
Lucas Santos <lucas.santos.silva2011@gmail.com>
2024-01-01 14:56:12 -0300

    Added remaining fields of AccountView

--- Apptizers/Screens/AccountView/AccountView.swift
+++ Apptizers/Screens/AccountView/AccountView.swift
@@ -1,15 +1,13 @@
-//
-//  AccountView.swift
-//  Apptizers
-//
-//  Created by Lucas Santos on 23/09/23.
-//
-
 import SwiftUI
 
 struct AccountView: View {
     @State private var firstName = ""
     @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
 
     var body: some View {
         NavigationView {
@@ -17,9 +15,29 @@ struct AccountView: View {
                 Section {
                     TextField("First Name", text: $firstName)
                     TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                    
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save changes")
+                    }
                 } header: {
                     Text("Personal information")
                 }
+                
+                Section {
+                    Toggle("Extra napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                } header: {
+                    Text("Request")
+                }
+                //.toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("Account")
         }
